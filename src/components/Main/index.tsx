82c79e0d782e983eb879251acceb107bc197634f
Mike Santos <iddeiasimplesmike@gmail.com>
2021-11-10 10:32:00 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado'
+}) => (
+  <S.Wrapper>
+    <S.Title>{title}</S.Title>
+  </S.Wrapper>
+)
+
+export default Main
---
+++ styled-components.d.ts
@@ -0,0 +1,6 @@
+import theme from 'styles/theme'
+
+declare module 'styled-components'{
+  type Theme = typeof theme
+  export interface DefaultTheme extends Theme{}
+}
